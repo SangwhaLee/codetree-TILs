af725b11d0cd7882532dca6f44b13f5cff053195
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-10 16:52:35 +0900

    Title: 사각형 채우기 2, Time: 210ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231110/사각형 채우기 2/rectangle-fill-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+let n = Number(fs.readFileSync(0).toString());
+const MOD = 10007;
+
+let dp = Array(1001).fill(0);
+
+dp[1] = 1;
+dp[2] = 3;
+
+for(let i=3;i<=1000;i++){
+    dp[i] = (dp[i-1]%MOD + dp[i-2]*3%MOD)%MOD;
+}
+
+console.log(dp[n]);
