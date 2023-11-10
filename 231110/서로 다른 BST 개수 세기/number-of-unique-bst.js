7b36658f917aaf23f812c6d442ede5f08bc11ef4
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-10 16:33:54 +0900

    Title: 서로 다른 BST 개수 세기, Time: 340ms, Memory: 15MB, Status: Passed - CodeTree

--- 231110/서로 다른 BST 개수 세기/number-of-unique-bst.js
+++ 231110/서로 다른 BST 개수 세기/number-of-unique-bst.js
@@ -4,16 +4,21 @@ let n = Number(fs.readFileSync(0).toString());
 
 let dp = Array(20).fill(0);
 
-dp[1] = 1;
-dp[2] = 2;
+const get_number_of_unique_bst = (n) => {
+    let number_of_unique_bst = 0;
 
-for(let i=3;i<=19;i++){
-    dp[i] += dp[i-1]*2;
-    tmp = 1;
-    for(let j=i-2;j>0;j--){
-        tmp *= j;
+    for(let i=0;i<n;i++){
+        number_of_unique_bst += dp[i] * dp[n - i - 1];
     }
-    dp[i] += tmp;
+
+    return number_of_unique_bst;
+}
+
+dp[0] = 1;
+dp[1] = 1;
+
+for(let i=2;i<=19;i++){
+    dp[i] = get_number_of_unique_bst(i);
 }
 
 console.log(dp[n])
