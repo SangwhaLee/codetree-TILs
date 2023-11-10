dd48a445842d601d14267938773a8838e4a525de
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-10 15:36:31 +0900

    Title: 사각형 채우기 3, Time: 241ms, Memory: 17MB, Status: Passed - CodeTree

--- 231110/사각형 채우기 3/rectangle-fill-3.js
+++ 231110/사각형 채우기 3/rectangle-fill-3.js
@@ -3,11 +3,16 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString());
 
 let memo = Array(1001).fill(0);
+const MOD = 1000000007;
 
+memo[0] = 1;
 memo[1] = 2;
 
 for(let i=2;i<=1000;i++){
-    memo[i] = (memo[i-1]*3 + 1);
+    memo[i] = (memo[i-1]*2 + memo[i-2]*3)%MOD;
+    for(let j=i-3;j>=0;j--){
+        memo[i] = (memo[i] + memo[j] * 2)%MOD;
+    }
 }
 
-console.log(memo[n]%1000000007);
+console.log(memo[n]);
