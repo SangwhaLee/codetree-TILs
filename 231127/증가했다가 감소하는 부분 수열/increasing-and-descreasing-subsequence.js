8698c3fe5f61f1c8e9bf8c7daefb6b1f6ec9695f
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-27 08:38:02 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 236ms, Memory: 13MB, Status: Wrong Answer - CodeTree

--- 231127/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.js
+++ 231127/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.js
@@ -6,34 +6,47 @@ let n = Number(input[0]);
 
 let arr = input[1].split(" ").map(Number);
 
-let dp = Array(n).fill(1);
-
-let way = Array(n).fill(0);
-
-let chance = Array(n).fill(true);
+let dp_up = Array(n).fill(1);
+let dp_down = Array(n).fill(1);
 
 for(let i=1;i<n;i++){
     for(let j=0;j<i;j++){
         if(arr[i] > arr[j]){
-            if(way[j] !== -1){
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-                way[i] = 1;
-            }
+            dp_up[i] = Math.max(dp_up[j]+1, dp_up[i]);
         }
-        else if(arr[i] < arr[j]){
-            if(way[j] === 1){
-                if(chance[j]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    way[i] = -1;
-                    chance[i] = false;
-                }
-            }
-            else{
-                dp[i] = Math.max(dp[i], dp[j] + 1);
-                way[i] = -1;
-            }
+        if(arr[i] < arr[j]){
+            dp_down[i] = Math.max(dp_down[j]+1, dp_down[i]);
         }
     }
 }
 
-console.log(Math.max(...dp));
+let maxUp = -1;
+let maxIdx = 0;
+let downMax = -1;
+let downIdx = 0;
+
+for(let i=0;i<n;i++){
+    if(dp_up[i] > maxUp){
+        maxUp = dp_up[i];
+        maxIdx = i;
+    }
+    if(dp_down[i] > downMax){
+        downMax = dp_down[i];
+        downIdx = i;
+    }
+}
+
+if(maxUp > downMax){
+    ans = maxUp;
+}
+else{
+    let tmp = maxUp;
+    for(let i =maxIdx+1;i<n;i++){
+        if(dp_down[i] > dp_down[maxIdx]){
+            tmp = Math.max(tmp, (dp_down[i] - dp_down[maxIdx]) + maxUp);
+        }
+    }
+    ans = Math.max(downMax, tmp);
+}
+
+console.log(ans);
