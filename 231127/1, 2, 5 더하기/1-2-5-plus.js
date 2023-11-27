f6eeea8968bcc95123e72d831fa429dcd9127474
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-27 12:56:06 +0900

    Title: 1, 2, 5 더하기, Time: 214ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231127/1, 2, 5 더하기/1-2-5-plus.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let n = Number(fs.readFileSync(0).toString());
+
+let dp = Array(n+1).fill(0);
+
+dp[0] = 1;
+
+for(let i=1;i<=n;i++){
+    dp[i] = dp[i-1];
+    if(i >= 2){
+        dp[i] += dp[i-2];
+    }
+    if(i >= 5){
+        dp[i] += dp[i-5];
+    }
+}
+
+console.log(dp[n]%10007);
