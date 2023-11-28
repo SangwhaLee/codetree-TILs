19f920326966a650cc690244c32dd8046c8c31cf
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-11-28 21:08:12 +0900

    Title: 최대 동전 거슬러주기, Time: 213ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231128/최대 동전 거슬러주기/max-coin-change.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().split("\n");
+
+let tmp = input[0].split(" ").map(Number);
+const n = tmp[0];
+const m = tmp[1];
+
+let coins = input[1].split(" ").map(Number);
+
+const min = Number.MIN_SAFE_INTEGER;
+
+let dp = Array(m+1).fill(min);
+
+dp[0] = 0;
+
+for(let i=1;i<=m;i++){
+    for(let j=0;j<i;j++){
+        if(dp[i-coins[j]] === min) continue;
+
+        if(i >= coins[j]){
+            dp[i] = Math.max(dp[i], dp[i-coins[j]] + 1);
+        }
+    }
+}
+
+console.log(dp[m]);
