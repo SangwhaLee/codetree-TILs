7cb2c3bc1204edca95ddbfc2bf50f1a2b46116d6
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-12-08 15:50:09 +0900

    Title: 비율 구하기, Time: 194ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 231208/비율 구하기/find-proportions.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+word = {}
+
+for _ in range(n):
+    tmp = input()
+
+    if tmp not in word:
+        word[tmp] = 1
+    else:
+        word[tmp] += 1
+
+for w in sorted(word.keys()):
+    tmp = round((100/n) * word[w], 5)
+    format_tmp = "{:.4f}".format(tmp)
+    print(w, format_tmp)
