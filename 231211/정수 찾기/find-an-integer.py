874fa3ec2fc313e5e23501a6ec75325bc0967c92
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-12-11 11:58:10 +0900

    Title: 정수 찾기, Time: 216ms, Memory: 37MB, Status: Passed - CodeTree

--- 231211/정수 찾기/find-an-integer.py
+++ 231211/정수 찾기/find-an-integer.py
@@ -5,9 +5,8 @@ m = int(input())
 arr2 = list(map(int,input().split()))
 
 st1 = set(arr)
-st2 = set(arr2)
 
-for elem in st2:
+for elem in arr2:
     if elem not in st1:
         print(0)
     else:
