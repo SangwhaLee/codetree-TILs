31626801be6b9ca3923d1f4cca328f4e837bbdb9
SangwhaLee <69105050+SangwhaLee@users.noreply.github.com>
2023-12-11 15:29:34 +0900

    Title: 대칭 차집합, Time: 266ms, Memory: 65MB, Status: Passed - CodeTree

--- 231211/대칭 차집합/symmetric-difference-set.py
+++ 231211/대칭 차집합/symmetric-difference-set.py
@@ -1,20 +1,8 @@
 a, b = map(int, input().split())
 
-a_arr = list(map(int,input().split()))
+a_arr = set(map(int,input().split()))
 
-b_arr = list(map(int, input().split()))
+b_arr = set(map(int, input().split()))
 
-both = []
-
-for aa in a_arr:
-    if aa in b_arr:
-        both.append(aa)
-
-ans = set(a_arr + b_arr)
-
-answer = 0
-for elem in ans:
-    if elem not in both:
-        answer += 1
-
-print(answer)
+answer = a_arr.symmetric_difference(b_arr)
+print(len(answer))
